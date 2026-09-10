fe2d39da263059d1604023e48d1b1af4d4484a0c
VinMan99 <v.manfredi@ersaf.it>
2026-09-10 17:20:31 +0200

    Data fine validazione si aggiorna in automatico

--- backend/src/listini_testa/routers.py
+++ backend/src/listini_testa/routers.py
@@ -11,10 +11,9 @@ from src.listino_tipoCorso.models import ListinoTipoCorsoDB
 from src.nome_universita.models import NomeUniversitaDB   
 from src.listini_dettagli.models import ListinoDettaglio
 from sqlalchemy.orm import joinedload, selectinload
-from src.auth.dipendenze import get_current_utente
 
 router = APIRouter(
-    prefix="/listini-testa", tags=["Listini Testa"],dependencies=[Depends(get_current_utente)],
+    prefix="/listini-testa", tags=["Listini Testa"]
 )
 
 # Funzione centralizzata per il calcolo del prossimo codice canonico
